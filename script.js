56b875b74873edcad442647b3bf4acf5bc59d74a
Diana Rios <31250962+dianario@users.noreply.github.com>
2023-01-29 22:50:17 +0000

    Begin work on playRound function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice () {
     const options = ["Rock", "Paper", "Scissors"];
-      let randomIndex = Math.floor(Math.random()*options.length);
+      const randomIndex = Math.floor(Math.random()*options.length);
       return options[randomIndex];
     }
     console.log(`Computer chooses: ${getComputerChoice ()} `)
